d2ac3e03acde6ee49fa6ab4ee6e1da535ebef974
Spuds <spuds@spudsdesign.com>
2023-03-17 13:53:43 -0500

    ! try and center the control

--- reCaptcha.template.php
+++ reCaptcha.template.php
@@ -6,5 +6,5 @@
 function template_verification_control_recaptcha()
 {
 	echo '
-				<div id="g-recaptcha" style="padding: 1em;"></div>';
+				<div id="g-recaptcha" style="width: 304px;margin: 0 auto;"></div>';
 }
